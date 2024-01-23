fd787e4888a494d4bb1425f15e528f907ce9a6f2
conejox <albertoconejerosa@gmail.com>
2024-01-24 00:09:03 +0100

    gallery

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
